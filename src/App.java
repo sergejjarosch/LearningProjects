ef54071eac61b593041aff18885ae3a754b5a294
sergejjarosch <sergejjarosch@gmail.com>
2024-02-01 15:35:21 +0100

    Update Red Panda

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+import java.io.IOException;
+
 public class App {
     /* The following main class is used to perform
     the various exercises that I create in the course
     of the learning process in order to clarify various
     basic functions for myself  */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 /* Trigonometrie
         Trigonometrie TrGn = new Trigonometrie();
         TrGn.TrGleichung();     */
@@ -20,5 +22,8 @@ public class App {
 The Math.random; method is used here.
         NumberGuessingGame NumGue = new NumberGuessingGame();
         NumGue.output_Guess();       */
+
+        ConfirmationWindow RedPand = new ConfirmationWindow();
+        RedPand.otput_ConWin();
     }
 }
---
+++ src/ConfirmationWindow.java
@@ -0,0 +1,29 @@
+import static javax.swing.JOptionPane.*;
+import javax.swing.ImageIcon;
+public class ConfirmationWindow {
+    int res;
+
+    public ConfirmationWindow() {
+        // Warning window
+        res = showConfirmDialog(null, // Parent window
+                "Do you want to see a red panda?", // Message
+                "Question", // Window designation
+                 YES_NO_OPTION // <- Buttons
+        );
+    }
+    public void otput_ConWin(){
+        //Simplified form of conditional branching
+        if( res == YES_NO_OPTION){
+            //Complete path to the image
+            String file = "src/img/csm_Nepalesischer_Roter_Panda_2_6323b83133.webp";
+            // Create object for the image
+            ImageIcon img = new ImageIcon(file);
+            // Show window with the image
+            showMessageDialog(null, // Parent window
+                    img , // Image
+                    "Red Panda", // Window designation
+                    PLAIN_MESSAGE // Pictogram not available
+                    );
+        }
+    }
+}
