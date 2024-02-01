60a967cd89b2461ea04b6f03701c7a51e766bc23
sergejjarosch <sergejjarosch@gmail.com>
2024-02-01 09:00:39 +0100

    SymbolKodierung erstellt

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) {
-        Trigonometrie TrGn = new Trigonometrie();
-        TrGn.TrGleichung();
+       /* Trigonometrie TrGn = new Trigonometrie();
+        TrGn.TrGleichung(); */
+        SymbolKodierung SymKord = new SymbolKodierung();
+
+        SymKord.OutputSymKord();
     }
 }
---
+++ src/SymbolKodierung.java
@@ -0,0 +1,26 @@
+import java.lang.foreign.*;
+
+public class SymbolKodierung {
+    // Code Number
+    int num;
+    // Output letters for the code
+    char A = 'A',B = 'B';
+    // Letters after decoding
+    char X, Y;
+
+    public SymbolKodierung() {
+        // First letters
+        num = ((int) B<<16) + ((int) A);
+    }
+    public void OutputSymKord () {
+        // Output
+        System.out.println("First letters: \'" + A + "\' and \'" + B + "\'." );
+        System.out.println("Code Number: " +num);
+        // decoding
+        Y = (char) (num >>> 16);
+        X = (char) (num^((int) Y << 16));
+        //output decoding result
+        System.out.println("Reverse conversion: ");
+        System.out.println("Letters \'" + X + "\' and \'" + Y + "\' .");
+    }
+}
