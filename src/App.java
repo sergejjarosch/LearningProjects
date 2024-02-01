6ade28ce16a97f331fa14a0b8e25a7346b06886f
sergejjarosch <sergejjarosch@gmail.com>
2024-02-01 09:09:27 +0100

    Comments specified

--- src/App.java
+++ src/App.java
@@ -1,9 +1,17 @@
 public class App {
+    /* The following main class is used to perform
+    the various exercises that I create in the course
+    of the learning process in order to clarify various
+    basic functions for myself  */
     public static void main(String[] args) {
-       /* Trigonometrie TrGn = new Trigonometrie();
-        TrGn.TrGleichung(); */
-        SymbolKodierung SymKord = new SymbolKodierung();
+// Trigonometrie
+/*      Trigonometrie TrGn = new Trigonometrie();
+        TrGn.TrGleichung();     */
 
-        SymKord.OutputSymKord();
+// Symbol Codierung
+/*      SymbolKodierung SymKord = new SymbolKodierung();
+        SymKord.OutputSymKord();    */
+
+
     }
 }
