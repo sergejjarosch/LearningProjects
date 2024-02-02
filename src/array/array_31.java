cd7d10aa321ceb15c2729e2e5ce18b5ece781de1
Sergej Jarosch <sergejjarosch@gmail.com>
2024-02-02 16:25:25 +0100

    add array

--- src/array/array_31.java
+++ src/array/array_31.java
@@ -1,4 +1,21 @@
 package array;
 
 public class array_31 {
+    int i, n;
+
+    public array_31(){
+        int [] data;
+        data = new int[] {3,8,1,7};
+        n = data.length;
+
+        int[] nums = new int[n];
+        // befüllung des zweiten Arrays
+        for (i = 0; i < nums.length; i++){
+            nums[i] = 2 * data[i] - 3;
+        }
+        public void array_32_out(){
+            System.out.println("nums[" + i + "] = " + nums[i]);
+        }
+
+    }
 }
